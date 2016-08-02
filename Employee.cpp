91503672736343b8de88339e0316374399f82c6b
jkates1 <jeremy.kates@charter.net>
2016-08-02 15:47:28 -0500

    finished Employee class

--- Employee.cpp
+++ Employee.cpp
@@ -9,13 +9,87 @@ using namespace std;
 
 namespace Records {
     Employee::Employee()
-    : mFirstName(" ")
-    , mLastName(" ")
+    : mFirstName("")
+    , mLastName("")
     , mEmployeeNumber(-1)
     , mSalary(kDefaultStartingSalary)
     , mHired(false)
     {
 
+    }
+
+    void Employee::promote(int raiseAmount)
+    {
+        setSalary(getSalary() + raiseAmount);
+    }
+    void Employee::demote(int demeritAmount)
+    {
+        setSalary(getSalary() - demeritAmount);
+    }
+
+    void Employee::hire()
+    {
+        mHired = true;
+    }
+
+    void Employee::fire()
+    {
+        mHired = false;
+    }
+
+    void Employee::display() const
+    {
+        cout << "Employee: " << getLastName() << ", " << getFirstName() << endl;
+        cout << "-------------------------" << endl;
+        cout << (mHired ? "Current Employee" : "Former Employee") << endl;
+        cout << "Employee Number: " << getEmployeeNumber() << endl;
+        cout << "Salary: $" << getSalary() << endl;
+        cout << endl;
+    }
+
+    void Employee::setFirstName(const string& firstName)
+    {
+        mFirstName = firstName;
+    }
+
+    const string& Employee::getFirstName() const
+    {
+        return mFirstName;
+    }
+
+    void Employee::setLastName(const std::string &lastName)
+    {
+        mLastName = lastName;
+    }
+
+    const string& Employee::getLastName() const
+    {
+        return mLastName;
+    }
+
+    void Employee::setEmployeeNumber(int employeeNumber)
+    {
+        mEmployeeNumber = employeeNumber;
+    }
+
+    int Employee::getEmployeeNumber() const
+    {
+        return mEmployeeNumber;
+    }
+
+    void Employee::setSalary(int newSalary)
+    {
+        mSalary = newSalary;
+    }
+
+    int Employee::getSalary() const
+    {
+        return mSalary;
+    }
+
+    bool Employee::getisHired() const
+    {
+        return mHired;
     }
 }
 
